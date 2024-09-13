df609ed231eb2e04c9879d0c540bae122c31cfea
Aleksandr <ponomarenko.100299@gmail.com>
2024-09-13 22:24:00 +0300

    fixed

--- mymath.go
+++ mymath.go
@@ -2,34 +2,18 @@ package mymath
 
 import "math"
 
-func Sqrt(x float64) float64 {
-	return math.Sqrt(x)
-}
-
-func Ceil(x float64) float64 {
-	return math.Ceil(x)
-}
-
-func Floor(x float64) float64 {
-	return math.Floor(x)
-}
-
-func Pow(x, y float64) float64 {
-	return math.Pow(x, y)
+func Abs(x float64) float64 {
+	return math.Abs(x)
 }
 
 func Max(x, y float64) float64 {
 	return math.Max(x, y)
 }
 
-func Min(x, y float64) float64 {
-	return math.Min(x, y)
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
 }
 
 func Yn(n int, x float64) float64 {
 	return math.Yn(n, x)
-}
-
-func Abs(x float64) float64 {
-	return math.Abs(x)
 }
